295b49ebf1cd2be663ef391e3c17f51dc789107c
yoeunes <younes.khoubza@gmail.com>
2017-02-14 01:01:34 +0000

    create Contest  Entity

---
+++ src/AppBundle/Entity/Contest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contest
+ *
+ * @ORM\Table(name="contest")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContestRepository")
+ */
+class Contest
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="duree", type="int")
+     */
+    private $duree;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Contest
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set duree
+     *
+     * @param int $duree
+     *
+     * @return Contest
+     */
+    public function setDuree($duree)
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    /**
+     * Get duree
+     *
+     * @return int
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+}
+
